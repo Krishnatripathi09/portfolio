55e4197b5a2fa6046dcc71e3e8729328c5364761
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-07-09 18:06:05 +0530

    feat(portfolio): Created a portfolio website

---
+++ src/Data.jsx
@@ -0,0 +1,535 @@
+import digiCosmetics from "./assets/digiCosmetics.png";
+import portfolio from "./assets/portfolio.png";
+import appleStore from "./assets/appleStore.png";
+import golsam from "./assets/golsam.png";
+
+import Testimonial1 from "./assets/testimonial1.jpg";
+import Testimonial2 from "./assets/testimonial2.jpg";
+import Testimonial3 from "./assets/testimonial3.jpg";
+import Testimonial4 from "./assets/testimonial4.jpg";
+import Testimonial5 from "./assets/testimonial5.jpg";
+
+export const links = [
+  {
+    name: "Home",
+    path: "home",
+  },
+  {
+    name: "Services",
+    path: "services",
+  },
+  {
+    name: "Skills",
+    path: "skills",
+  },
+  {
+    name: "Projects",
+    path: "projects",
+  },
+  {
+    name: "Resume",
+    path: "resume",
+  },
+  {
+    name: "Testimonials",
+    path: "testimonial",
+  },
+  {
+    name: "Pricing",
+    path: "pricing",
+  },
+  {
+    name: "Blog",
+    path: "blog",
+  },
+  {
+    name: "Contact",
+    path: "contact",
+  },
+];
+
+export const service = [
+  {
+    id: 1,
+    name: "Web Development",
+    title: "Custom Development",
+    description:
+      "I build stunning websites using React.js, Next.js, and modern JavaScript that aren't just beautiful, but also fast and interactive. Your ideas deserve a website that stands out from the crowd!",
+  },
+  {
+    id: 2,
+    name: "User Experience",
+    title: "User Experience Design",
+    description:
+      "Let me craft an experience your users will love! I create intuitive, engaging interfaces that not only look great but feel natural to use, keeping your visitors coming back for more.",
+  },
+  {
+    id: 3,
+    name: "Web Optimization",
+    title: "Speed Optimization",
+    description:
+      "Is your website crawling when it should be flying? I'll tune up your site's performance so it loads in a flash, boosting your SEO rankings and keeping impatient visitors from clicking away.",
+  },
+  {
+    id: 4,
+    name: "Web Maintenance",
+    title: "Maintenance and Support",
+    description:
+      "Your website needs love too! I'll handle all the behind-the-scenes work—bug fixes, security updates, and content refreshes—so you can focus on what you do best: growing your business.",
+  },
+  {
+    id: 5,
+    name: "Interactive Effects",
+    title: "Animations and Effects",
+    description:
+      "Want to wow your visitors? I'll bring your website to life with eye-catching animations and interactive elements using CSS, framer-motion, and three.js that will leave a lasting impression.",
+  },
+];
+
+export const skill = [
+  {
+    id: 1,
+    name: "Express-Js",
+    percentage: 99,
+    description:
+      "Hypertext Markup Language (HTML) is the standard markup language used to create web pages and web applications.",
+  },
+
+  {
+    id: 2,
+     name: "NodeJS",
+    percentage: 90,
+    description:
+      "JavaScript is a scripting language used for creating dynamic web pages and web applications.",
+  },
+
+  // {
+  //   id: 3,
+  //   name: "React.js",
+  //   percentage: 90,
+  //   description:
+  //     "A popular JavaScript library for building user interfaces, React is used to create dynamic and responsive web applications.",
+  // },
+
+  {
+    id: 4,
+    name: "Mongo DB",
+    percentage: 85,
+    // description:
+    //   "Next.js is a React-based framework for building server-side rendered and statically generated web applications.",
+  },
+
+  // {
+  //   id: 5,
+  //   name: "TypeScript",
+  //   percentage: 75,
+  //   description:
+  //     "TypeScript is a superset of JavaScript that adds optional static typing and other features to the language.",
+  // },
+
+  // {
+  //   id: 6,
+  //   name: "Tailwind CSS",
+  //   percentage: 80,
+  //   description:
+  //     "Tailwind CSS is a utility-first CSS framework that provides developers with a set of pre-built CSS classes for styling web applications.",
+  // },
+
+  {
+    id: 7,
+    name: "Git",
+    percentage: 80,
+    description:
+      "Git is a widely used version control system that is essential for collaborative development and project management. ",
+  },
+
+  // {
+  //   id: 8,
+  //   name: "Webpack",
+  //   percentage: 50,
+  //   description:
+  //     "Webpack is a module bundler for modern JavaScript applications. It bundles assets, modules, and dependencies into a single file.",
+  // },
+
+  // {
+  //   id: 9,
+  //   name: "GraphQL",
+  //   percentage: 80,
+  //   description:
+  //     "GraphQL is a query language for APIs that provides a more efficient, powerful and flexible alternative to RESTful APIs.",
+  // },
+
+  {
+    id: 10,
+    name: "RESTful API's",
+    percentage: 90,
+    description:
+      "RESTful API is an interface that two computer systems use to exchange information securely over the internet.",
+  },
+
+  // {
+  //   id: 11,
+  //   name: "Sass",
+  //   percentage: 75,
+  //   description:
+  //     "Sass is a CSS preprocessor that allows developers to write CSS in a more concise and structured way.",
+  // },
+
+  // {
+  //   id: 12,
+  //   name: "Bootstrap",
+  //   percentage: 85,
+  //   description:
+  //     "Bootstrap is a popular CSS framework that provides developers with a set of pre-built UI components.",
+  // },
+
+  // {
+  //   id: 13,
+  //   name: "Redux",
+  //   percentage: 80,
+  //   description:
+  //     "Redux is a predictable state container for JavaScript apps, used to manage the state of complex applications.",
+  // },
+
+  // {
+  //   id: 14,
+  //   name: "Zustand",
+  //   percentage: 75,
+  //   description:
+  //     "Zustand is a small, fast and scalable status management solution. Its state management is centralized and action-based.",
+  // },
+
+  // {
+  //   id: 15,
+  //   name: "Jest & Cypress",
+  //   percentage: 45,
+  //   description:
+  //     "Jest and Cypress are JavaScript-based testing framework designed to ensure correctness of any JavaScript codebase.",
+  // },
+
+  // {
+  //   id: 16,
+  //   name: "Playwright",
+  //   percentage: 75,
+  //   description:
+  //     "Playwright is a modern end-to-end testing framework that enables reliable cross-browser automation for web applications.",
+  // },
+
+  // {
+  //   id: 17,
+  //   name: "React Query",
+  //   percentage: 80,
+  //   description:
+  //     "React Query is a powerful data-fetching and state management library for React applications that simplifies server state handling.",
+  // },
+
+  // {
+  //   id: 18,
+  //   name: "React Hook Form",
+  //   percentage: 75,
+  //   description:
+  //     "React Hook Form is a performant, flexible and extensible forms library with easy-to-use validation for React applications.",
+  // },
+
+  {
+    id: 19,
+    name: "Docker",
+    percentage: 40,
+    description:
+      "Docker is a platform for developing, shipping, and running applications in containers, making it easier to create and deploy applications consistently.",
+  },
+
+  {
+    id: 20,
+      name: "JavaScript",
+    percentage: 75,
+    // description:
+    //   "Three.js is a cross-browser JavaScript library used to create and display animated 3D computer graphics in web browsers using WebGL.",
+  },
+
+  {
+    id: 21,
+  name: "HTML",
+    percentage: 55,
+    description:
+      "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine that allows developers to run JavaScript on the server side.",
+  },
+
+  // {
+  //   id: 22,
+  //   name: "MaterialUI",
+  //   percentage: 75,
+  //   description:
+  //     "Material-UI is a popular React UI framework that implements Google's Material Design principles with a comprehensive suite of components.",
+  // },
+
+  // {
+  //   id: 23,
+  //   name: "RadixUI",
+  //   percentage: 75,
+  //   description:
+  //     "Radix UI is a low-level UI component library with a focus on accessibility, customization and developer experience.",
+  // },
+
+  // {
+  //   id: 24,
+  //   name: "ANTDESIGN",
+  //   percentage: 85,
+  //   description:
+  //     "Ant Design is an enterprise-class UI design language and React UI library with a set of high-quality components and demos.",
+  // },
+];
+
+export const project = [
+  {
+    id: 1,
+    img: digiCosmetics,
+    category: "Landing",
+    title: "digiCosmetics",
+    description:
+      "Landing page designed for selling cosmetic products. It typically includes a visually appealing design, easy navigation, and clear product descriptions.",
+    url: "https://digicosmetics.netlify.app",
+    sourceUrl: "https://github.com/khakiiman/digiCosmetics",
+  },
+
+  {
+    id: 2,
+    img: portfolio,
+    category: "Portfolio",
+    title: "Developer Portfolio",
+    description:
+      "This Portfolio involves creating a visually appealing and user-friendly website that showcases the skills and portfolio of a frontend developer.",
+    url: "http://imankhaki.netlify.app/",
+    sourceUrl: "https://github.com/khakiiman/portfolio",
+  },
+
+  {
+    id: 3,
+    img: appleStore,
+    category: "Ecommerce",
+    title: "New Apple Store",
+    description:
+      "The new Apple Store landing page is a web page designed for showcasing and promoting Apple products, services, and features.",
+    url: "https://newapplestore.netlify.app/",
+    sourceUrl: "https://github.com/khakiiman/applestore",
+  },
+
+  {
+    id: 4,
+    img: golsam,
+    category: "Blog",
+    title: "Golsam Sepahan",
+    description:
+      "A new design blog provides informative content related to the agricultural industry, including tips and advice on the use of fertilizers and pesticides. ",
+    url: "https://golsamsepahan.netlify.app",
+    sourceUrl: "https://github.com/khakiiman/golsam-sepahan",
+  },
+];
+
+export const cv = [
+  {
+    id: 1,
+    title: "University of Tehran",
+    subtitle: "M.Sc. in Engineering",
+    date: "2014 - 2016",
+    description:
+      "The Master of Science in Hydraulic Engineering program at the University of Tehran is a two-year postgraduate degree that provides students with advanced knowledge of water structural engineering.",
+    category: "education",
+  },
+
+  {
+    id: 2,
+    title: "Mehregan Institute Certificate",
+    subtitle: "SQL Server",
+    date: "2012 - 2013",
+    description:
+      "The SQL Server Certificate program at Mehregan Institute is a training program that provides students with advanced knowledge of SQL Server databases, including database design, administration, and programming.",
+    category: "education",
+  },
+
+  {
+    id: 3,
+    title: "Mehregan Institute Certificate",
+    subtitle: "C#.Net",
+    date: "2012 - 2013",
+    description:
+      "The C#.Net Certificate program at Mehregan Institute is a training program that provides students with advanced knowledge of C# programming language and .NET framework.",
+    category: "education",
+  },
+
+  {
+    id: 4,
+    title: "Coming Soon...",
+    subtitle: "Coming Soon...",
+    date: "0000 - 0000",
+    description: "Coming Soon...",
+    category: "education",
+  },
+
+  {
+    id: 5,
+    title: "Freelancing (Web Development)",
+    subtitle: "GolsamSepahan Blog",
+    date: "2022 - PRESENT",
+    description:
+      "As a freelance frontend developer, I was responsible for developing the frontend of a blog website from scratch, which involved creating an intuitive and visually engaging user interface. My responsibilities included designing and implementing responsive layouts, optimizing website performance, and ensuring cross-browser compatibility.",
+    category: "experience",
+  },
+
+  {
+    id: 6,
+    title: "Coming Soon...",
+    subtitle: "Coming Soon...",
+    date: "000 - 0000",
+    description: "Coming Soon....",
+    category: "experience",
+  },
+
+  {
+    id: 7,
+    title: "Coming Soon...",
+    subtitle: "Coming Soon...",
+    date: "000 - 0000",
+    description: "Coming Soon....",
+    category: "experience",
+  },
+
+  {
+    id: 8,
+    title: "Coming Soon...",
+    subtitle: "Coming Soon...",
+    date: "000 - 0000",
+    description: "Coming Soon....",
+    category: "experience",
+  },
+];
+
+export const testimonial = [
+  {
+    id: 1,
+    img: Testimonial1,
+    name: "Barbara Wilson",
+    author: "CEO Company",
+    description: "Coming Soon...",
+  },
+  {
+    id: 2,
+    img: Testimonial2,
+    name: "Charlie Smith",
+    author: "Designer",
+    description: "Coming Soon...",
+  },
+  {
+    id: 3,
+    img: Testimonial3,
+    name: "Roy Wang",
+    author: "Manager GYM",
+    description: "Coming Soon...",
+  },
+  {
+    id: 4,
+    img: Testimonial4,
+    name: "Jennifer Smith",
+    author: "CEO & Founder",
+    description: "Coming Soon...",
+  },
+  {
+    id: 5,
+    img: Testimonial5,
+    name: "Paul Freeman",
+    author: "Photographer",
+    description: "Coming Soon...",
+  },
+];
+
+export const post = [
+  {
+    id: 1,
+    date: "October 31, 2022",
+    imgURL:
+      "https://www.freecodecamp.org/news/content/images/size/w2000/2022/03/freeCodeCamp-Cover.png",
+    title: "Introduction to React Hooks",
+    description:
+      "Learn how to use React Hooks to simplify state management and improve performance in your frontend applications.",
+  },
+  {
+    id: 2,
+    date: "November 19, 2022",
+    imgURL: "https://softagency.in/wp-content/uploads/2023/02/vuejsfeat.jpg",
+    title: "Getting Started with Vue.js",
+    description:
+      "Explore the basics of Vue.js, a progressive JavaScript framework for building user interfaces.",
+  },
+  {
+    id: 3,
+    date: "April 12, 2022",
+    imgURL:
+      "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*qF8LfAwUhl57g9T0BVvVdg.jpeg",
+    title: "Best Practices for Responsive Web Design",
+    description:
+      "Discover the key principles and techniques for creating responsive web designs that look great on any device.",
+  },
+  {
+    id: 4,
+    date: "March 5, 2022",
+    imgURL:
+      "https://res.cloudinary.com/practicaldev/image/fetch/s--CHCog3Bn--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://cdn-images-1.medium.com/max/3200/1%2AtiS4RzayAJdIeTEkNL1ztw.png",
+    title: "Mastering CSS Flexbox",
+    description:
+      "Get up to speed with CSS Flexbox, a powerful layout tool for building modern, flexible, and responsive web designs.",
+  },
+  {
+    id: 5,
+    date: "June 25, 2022",
+    imgURL:
+      "https://silktide.com/app/uploads/2021/05/We-analyzed-6554-websites-1024x576.jpg",
+    title: "Creating Accessible Web Content",
+    description:
+      "Learn how to make your web content more accessible to users with disabilities, including tips for designing for screen readers and other assistive technologies.",
+  },
+  {
+    id: 6,
+    date: "November 19, 2022",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6OyXMz4TaaY4FJOg00N6c7zsYKuqAAA8VUA&usqp=CAU",
+    title: "10 Tips for Writing Clean Code in JavaScript",
+    description:
+      "Discover best practices for writing clean, maintainable, and efficient JavaScript code that will help you become a better developer.",
+  },
+  {
+    id: 7,
+    date: "November 19, 2022",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_lU_0yLYzO5o9oKAeShqM-8y_s1BWudtcyQ&usqp=CAU",
+    title: "The Future of Frontend Development: What to Expect in 2022",
+    description:
+      "Explore the latest trends and technologies in frontend development.",
+  },
+  {
+    id: 8,
+    date: "November 19, 2022",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQANuqChNWRh2a5AbtgPZYPnAogzqDsBQ-dAg&usqp=CAU",
+    title: "10 Essential Tools for Every Frontend Developer",
+    description:
+      "Check out our list of must-have tools for frontend developers, from code editors.",
+  },
+  {
+    id: 9,
+    date: "November 19, 2022",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxpPs_cWDHKLvC45gy0btz4sNfD9LOArjcdA&usqp=CAU",
+    title: "Mastering the Fundamentals of Web Development",
+    description:
+      "Learn the basics of web development, including HTML, CSS, and JavaScript, and start building your own web applications from scratch.",
+  },
+  {
+    id: 10,
+    date: "November 19, 2022",
+    imgURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7KXJ50sZkMTcGcKBRpGj3Vnwz8g6iTnqKSQ&usqp=CAU",
+    title: "Creating Beautiful and Responsive UI Designs with CSS Grid",
+    description:
+      "Get up to speed with CSS Grid, a powerful layout tool for creating complex and responsive user interfaces that adapt to any screen size.",
+  },
+];
